c79856a5fb7eb9b14250fca84a9bfdb24e98965a
tfang0252 <32147688+tfang0252@users.noreply.github.com>
2018-03-12 12:29:19 -0400

    listview
    can add players
    still need to implement delete and edit

--- app/src/main/java/club/elitesocceracademy/elitesocceracademy/MainMenu.java
+++ app/src/main/java/club/elitesocceracademy/elitesocceracademy/MainMenu.java
@@ -1,7 +1,12 @@
 package club.elitesocceracademy.elitesocceracademy;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        rosterButton();
+    }
+
+    private void rosterButton() {
+        ImageButton btn = (ImageButton) findViewById(R.id.rosterBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, RosterActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/club/elitesocceracademy/elitesocceracademy/RosterActivity.java
@@ -0,0 +1,171 @@
+package club.elitesocceracademy.elitesocceracademy;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+import android.util.*;
+import android.*;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ListAdapter;
+import java.util.ArrayList;
+import java.util.List;
+import android.app.AlertDialog;
+import android.text.InputType;
+import android.content.DialogInterface;
+import java.util.ArrayList;
+
+public class RosterActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ArrayList<String> list = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_roster);
+        SwipeMenuListView listView = (SwipeMenuListView) findViewById(R.id.listView);
+
+
+        list.add("");
+        list.add("Vanessa Flores");
+        list.add("Kassandra Gonzales");
+        list.add("Ava Haick");
+        list.add("Skylar Hubbard");
+        list.add("Maryah Maldonado");
+        list.add("Ellana Mena");
+        list.add("Mackenzie Molina");
+        list.add("Ashlyn Morales");
+        list.add("Rebekah Strickland");
+        list.add("Paige Troyer");
+
+
+        ArrayAdapter adapter = new ArrayAdapter(RosterActivity.this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0x00, 0x66,
+                        0xff)));
+                // set item width
+                openItem.setWidth(170);
+                // set item title
+                openItem.setTitle("Edit");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(170);
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_trash);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+        listView.setMenuCreator(creator);
+
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+                        Log.d(TAG, "onMenuItemClick: clicked item " + index);
+                        break;
+                    case 1:
+                        Log.d(TAG, "onMenuItemClick: clicked item " + index);
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
+    }
+
+    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+    public void addItems(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Player Name");
+
+// Set up the input
+        final EditText input = new EditText(this);
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+// Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String playerName = input.getText().toString();
+                list.add(playerName);
+                System.out.println(playerName);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
+
+
+    }
+}
+
+
+
